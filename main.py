4381a1df878feef501fde6d2791cf61f2fe7ad87
Miami-stack <muratajmedov@gmail.com>
2020-12-14 11:41:43 +0500

    changes

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 """Скрипт подсчета лунных кратеров."""
 
 import os
-import warnings
+import sys
+from typing import Any
 
 import numpy as np
 
 
-def get_value_from_file(file: str) -> np.ndarray:
+def get_value_from_file(file: str) -> object:
     """Читает файл и преобразовывает его в 2д массив."""
+
     base_dir = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(base_dir, file)
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        my_file = np.loadtxt(file_path).astype(int)
-        return my_file
+    if os.stat(file_path).st_size == 0:
+        print("Файл пустой")
+        sys.exit(0)
+    my_file = np.loadtxt(file_path).astype(int).tolist()
+    return my_file
 
 
-def calculate(matrix1: np.ndarray, query1: np.ndarray) -> str:
+def calculate(matrix1: Any, query1: list) -> int:
     """Эта функция находит и подсчитывает кратеры."""
     searchlist = []
-    t = all(matrix1[0, :] == 1)
-    if t:
-        searchlist.append(t)
+    if all(matrix1[matrix1[0][:] == 1]):
+        searchlist.append(1)
     for row in matrix1:
         search = all(x in row for x in query1)
         if search is True:
             searchlist.append(search)
 
-    return f"Количество краторов {len(searchlist)}"
+    return len(searchlist)
 
 
 matrix = get_value_from_file('test_file')
-query = np.array([1, 0])
+query = [1, 0]
 
-print(calculate(matrix, query))
+print(f"Количество кратеров: {calculate(matrix, query)}")
