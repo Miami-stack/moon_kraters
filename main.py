1acc4db702164a5eee60e835c0fc84cf362340e0
Miami-stack <muratajmedov@gmail.com>
2020-12-14 21:09:57 +0500

    changes

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 
 import os
 import sys
-from typing import Any
 import numpy as np
 
 
-def get_value_from_file(file: str) -> object:
+def get_value_from_file(file: str) -> list:
     """Читает файл и преобразовывает его в 2д массив."""
-
     base_dir = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(base_dir, file)
     if os.stat(file_path).st_size == 0:
@@ -18,13 +16,13 @@ def get_value_from_file(file: str) -> object:
     return my_file
 
 
-def calculate(matrix1: Any, query1: list) -> int:
+def calculate(matrix1: list) -> int:
     """Эта функция находит и подсчитывает кратеры."""
     searchlist = []
     if all(matrix1[matrix1[0][:] == 1]):
         searchlist.append(1)
     for row in matrix1:
-        search = all(x in row for x in query1)
+        search = all(x in row for x in [0,1])
         if search is True:
             searchlist.append(search)
 
@@ -32,6 +30,7 @@ def calculate(matrix1: Any, query1: list) -> int:
 
 
 matrix = get_value_from_file('test_file')
-query = [1, 0]
+print(matrix)
 
-print(f"Количество кратеров: {calculate(matrix, query)}")
+
+print(f"Количество кратеров: {calculate(matrix)}")
